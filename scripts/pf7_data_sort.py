2f6408ba715b0b2aeaa368beb2faf31596444f93
Clara Rehmann <crehmann@uoregon.edu>
2023-04-14 21:37:26 +0000

    anopheles and plasmodium download and sorting

---
+++ scripts/pf7_data_sort.py
@@ -0,0 +1,15 @@
+import pandas as pd, numpy as np, os
+
+# only individuals that pass QC and are in africa
+xlim = (-31.3, 58)
+ylim = (-37.5, 38.8)
+
+pfpath = '/sietch_colab/crehmann/pf7/Pf7_samples.txt'
+outpath = 'data/pf7/'
+
+df = pd.read_csv(pfpath, sep='\t')
+df = df[df['QC pass'] == True].reset_index(drop=True)
+df['x'] = df['Admin level 1 longitude']
+df['y'] = df['Admin level 1 latitude']
+df = df[(xlim[0] < df.x) & (xlim[1] > df.x) & (ylim[0] < df.y) & (ylim[1] > df.y)]
+df.to_csv(os.path.join(outpath, 'pf7_africa_QC.txt'))
