98fc00afff75ac3dfda6a786bfff31443eb401e4
clararehmann <crehmann@uoregon.edu>
2023-04-14 14:33:02 -0700

    adding Locating to slimulation snakefile

--- scripts/sample_treeseq.py
+++ scripts/sample_treeseq.py
@@ -7,21 +7,23 @@ import subprocess
 
 training_sets = ['uniform', 'point', 'half']
 Nreps = 1
-skews = [0.5]#, 0.6, 0.7, 0.8, 0.9]
+skews = [0.5, 0.6, 0.7, 0.8, 0.9]
 tssize = 450
 
 outpath = 'out/'
 metadatapath = 'out/metadata/'
 vcfpath = 'out/genotypes/'
-uniformpath, pointpath, halfpath = [f'out/training_sets/{ts}' for ts in training_sets]
-for f in [outpath, metadatapath, vcfpath, uniformpath, pointpath, halfpath]:
+tspath = 'out/training_sets/'
+for f in [outpath, metadatapath, vcfpath, tspath]:
     subprocess.run(f'mkdir -p {f}', shell=True)
 
 tree = sys.argv[1]
 sim, sigma, mu, seed = np.array(tree.split('/')[-1].replace('.trees','').split('_'))[[0,2,4,5]].astype(float)
+sim = int(sim)
+seed = int(seed)
 
 # process each tree sequence 10x
-for n in range(10):
+for n in range(Nreps):
     ts = tskit.load(tree)
     ts = process_treeseq(ts)
     
@@ -30,18 +32,18 @@ for n in range(10):
     # sample uniformly
     uniform_df = sample_ts(ts, 'uniform', tssize, max_distance=1.5)
     all_samples.extend(uniform_df.sampleID)
-    uniform_df.to_csv(os.path.join(uniformpath, f'{sim}_sigma_{sigma}_mu_{mu}_{seed}_uniform_{n}.txt'), sep='\t')
+    uniform_df.to_csv(os.path.join(tspath, f'{sim}_sigma_{sigma}_mu_{mu}_uniform_{n}.txt'), sep='\t')
     
     for skew in skews:
         # point sampling
         point_df = sample_ts(ts, 'point', tssize, point=(40,40), decay=1/skew)
         all_samples.extend(point_df.sampleID)
-        point_df.to_csv(os.path.join(pointpath, f'{sim}_sigma_{sigma}_mu_{mu}_{seed}_point_{n}.txt'), sep='\t')
+        point_df.to_csv(os.path.join(tspath, f'{sim}_sigma_{sigma}_mu_{mu}_point_{skew}_{n}.txt'), sep='\t')
         
         # biased sampling
         half_df = sample_ts(ts, 'half', tssize, skew=skew)
         all_samples.extend(half_df.sampleID)
-        half_df.to_csv(os.path.join(halfpath, f'{sim}_sigma_{sigma}_mu_{mu}_{seed}_half_{n}.txt'), sep='\t')
+        half_df.to_csv(os.path.join(tspath, f'{sim}_sigma_{sigma}_mu_{mu}_half_{skew}_{n}.txt'), sep='\t')
 
     # simplify tree sequence to only relevant individuals
     all_samples = np.unique(all_samples)
@@ -53,10 +55,10 @@ for n in range(10):
     
     # save individual locations
     metadata = sample_locations(ts)
-    metadata.to_csv(os.path.join(metadatapath, f'{sim}_sigma_{sigma}_mu_{mu}_{seed}_{n}.txt'), sep='\t')
+    metadata.to_csv(os.path.join(metadatapath, f'{sim}_sigma_{sigma}_mu_{mu}_{n}.txt'), sep='\t')
     
-    print(type(ts))
     # save genotypes
-    treepath = os.path.join(vcfpath, f'{sim}_sigma_{sigma}_mu_{mu}_{seed}_{n}.trees')
-    ts.write_vcf(output=treepath, 
-                 individual_names=all_samples)
+    treepath = os.path.join(vcfpath, f'{sim}_sigma_{sigma}_mu_{mu}_{n}.vcf')
+    
+    with open(treepath, 'w') as f:
+        ts.write_vcf(f, individual_names=all_samples)
